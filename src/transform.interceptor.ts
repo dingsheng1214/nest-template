0083db6f04885437fd1e794df786e35a95c56688
dingsheng <dingduoduo0916@gmail.com>
2022-07-13 08:25:10 +0800

    feat: 统一响应体封装

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        status: 0,
+        extra: {},
+        message: 'success',
+        success: true,
+      })),
+    );
+  }
+}
