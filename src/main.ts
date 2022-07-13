e320ef217200d38d103d6cfc654ee39b36fc339a
dingsheng <dingduoduo0916@gmail.com>
2022-07-13 11:01:37 +0800

    feat: HRM 热更新

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 import { BaseExceptionFilter } from './common/exceptions/filters/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/filters/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,6 +17,13 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   // 注意先后顺序
   app.useGlobalFilters(new BaseExceptionFilter(), new HttpExceptionFilter());
+
+  // HRM 热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
   await app.listen(3000);
 }
+
 bootstrap();
